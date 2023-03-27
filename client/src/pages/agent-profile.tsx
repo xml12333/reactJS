74a12fab8332421718204a3fa0285e9ea8a62e00
Nik T <xml12333@gmail.com>
2023-03-27 08:53:45 +0300

    refine-fullstack-dashboard

--- client/src/pages/agent-profile.tsx
+++ client/src/pages/agent-profile.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useOne } from "@pankod/refine-core";
+import { useParams } from "@pankod/refine-react-router-v6";
+
+import { Profile } from "components";
 
 const AgentProfile = () => {
-  return (
-    <div>agent-profile</div>
-  )
-}
+    const { id } = useParams();
 
-export default AgentProfile
+    const { data, isLoading, isError } = useOne({
+        resource: "users",
+        id: id as string,
+    });
+
+    console.log(data);
+
+    const myProfile = data?.data ?? [];
+
+    if (isLoading) return <div>loading...</div>;
+    if (isError) return <div>error...</div>;
+
+    return (
+        <Profile
+            type="Agent"
+            name={myProfile.name}
+            email={myProfile.email}
+            avatar={myProfile.avatar}
+            properties={myProfile.allProperties}
+        />
+    );
+};
+
+export default AgentProfile;
