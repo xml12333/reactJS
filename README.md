e05fc00340dca3c2a35ea362d7a6e023ce8b42d8
Nik T <xml12333@gmail.com>
2023-08-15 11:09:10 +0300

    42-etan-portfolio

---
+++ README.md
@@ -0,0 +1,8 @@
+# Next.js 13 Portfolio
+
+```
+npx create-next-app@latest
+```
+
+npm i @next/font,framer-motion,next,react,react-countup,react-dom,react-icons,react-tsparticles,swiper,tailwind-scrollbar,tsparticles,
+autoprefixer, eslint, eslint-config-next, postcss, tailwindcss
---
+++ components/Socials.js
@@ -0,0 +1,53 @@
+import Link from "next/link";
+import {
+  RiYoutubeLine,
+  RiInstagramLine,
+  RiFacebookBoxLine,
+  RiDribbbleLine,
+  RiPinterestLine,
+  RiBehanceLine,
+} from "react-icons/ri";
+const Socials = () => {
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link
+        href={""}
+        className="hover:text-accent, transition-all duration-300"
+      >
+        <RiYoutubeLine />
+      </Link>
+      <Link
+        href={""}
+        className="hover:text-accent, transition-all duration-300"
+      >
+        <RiFacebookBoxLine />
+      </Link>
+      <Link
+        href={""}
+        className="hover:text-accent, transition-all duration-300"
+      >
+        <RiInstagramLine />
+      </Link>
+      <Link
+        href={""}
+        className="hover:text-accent, transition-all duration-300"
+      >
+        <RiDribbbleLine />
+      </Link>
+      <Link
+        href={""}
+        className="hover:text-accent, transition-all duration-300"
+      >
+        <RiPinterestLine />
+      </Link>
+      <Link
+        href={""}
+        className="hover:text-accent, transition-all duration-300"
+      >
+        <RiBehanceLine />
+      </Link>
+    </div>
+  );
+};
+
+export default Socials;
